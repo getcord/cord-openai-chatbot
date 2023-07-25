77a6205988b393283c820383d9073ab7654e601f
Jackson Gabbard <jg@jg.gg>
2023-07-25 19:57:32 +0100

    Document the conversation caches a bit

--- server/lib/cache.ts
+++ server/lib/cache.ts
@@ -3,6 +3,11 @@
 import { readFileSync } from 'fs';
 import { BOT_SAFE_WORD, BOT_USER_NAME, CONTENT_START } from './bot';
 
+// These caches are pretty limited, but work okay. In a real-world context,
+// you'd probably want to replace this with something a bit more robust like a
+// Redis caching layer or something similar. These caches make it easy to
+// reassemble the messages in the conversation when requesting the next
+// completion from the LLM.
 export const conversationCache: { [threadID: string]: string } = {};
 export const threadOrgCache: { [threadID: string]: string } = {};
 
